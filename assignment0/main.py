231bdfe75e7a87f952931ed075ce1bbb1672d5af
99004327-Sourabh <Kannada@007>
2024-02-06 20:26:07 -0500

    test download was not required

--- assignment0/main.py
+++ assignment0/main.py
@@ -29,29 +29,38 @@ def extract_incidents(pdf_path):
             rect_incident_number = fitz.Rect(150.86000061035156, block_rect.y0, 229.82000732421875, block_rect.y1)
             rect_location = fitz.Rect(229.82000732421875, block_rect.y0, 423.1900244140625, block_rect.y1)
             rect_nature = fitz.Rect(423.1900244140625, block_rect.y0, 623.8599853515625, block_rect.y1)
+            rect_ori = fitz.Rect(623.859985351562, block_rect.y0, block_rect.x1, block_rect.y1)
 
             # Extract text for each field
             date_time_text = page.get_text("text", clip=rect_date_time).strip()
             incident_number_text = page.get_text("text", clip=rect_incident_number).strip()
             location_text = page.get_text("text", clip=rect_location).strip()
             nature_text = page.get_text("text", clip=rect_nature).strip()
-
+            ori = page.get_text("text", clip=rect_ori).strip()
+            print(date_time_text, incident_number_text, location_text, nature_text, ori)
+            #
             if date_time_text and incident_number_text and location_text and nature_text:
                 incidents.append((date_time_text, incident_number_text, location_text, nature_text,
                                   ""))  # "" as placeholder for incident_ori
+    print(incidents)
     return incidents
 
 
 # Function to create the SQLite database
 def create_db():
+    # Define the database path
+    db_path = os.path.join('resources', 'normandb.db')
+
+    if os.path.exists(db_path):
+        os.remove(db_path)
     # Ensure the resources directory exists
     os.makedirs('resources', exist_ok=True)
 
-    # Connect to the SQLite database within the resources directory
-    db_path = os.path.join('resources', 'normandb.db')
+
+    # Connect to the SQLite database (this will create a new database)
     conn = sqlite3.connect(db_path)
 
-    # Create the incidents table if it doesn't exist
+    # Create the incidents table
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS incidents (
                     incident_time TEXT,
@@ -62,7 +71,6 @@ def create_db():
                  )''')
     conn.commit()
     return conn
-
 
 # Function to insert incidents into the database
 def populate_db(conn, incidents):
